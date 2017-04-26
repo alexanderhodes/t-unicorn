6740c9ca71b76bb2e02312689068d0bb65fd8d04
lizakanitz <Kudinova22>
2017-04-27 01:06:12 +0200

    1

---
+++ client/JS/temp_structure.js
@@ -0,0 +1,77 @@
+/**
+ * Created by lizakanitz on 25.04.17.
+ */
+/**
+ * Created by lizakanitz on 23.04.17.
+ */
+
+
+var statement1={
+  "statement_id": "1",
+  "statement_text": "Es besteht Bedarf einer Anpassung der Geschäftsprozesse an die Cloud.",
+  "options": [{"option_id": "1", "result_id": "1"}, {"option_id": "2", "result_id": "3"},
+    {"option_id": "3", "result_id": "1"},{"option_id": "4", "result_id": "1"},
+    {"option_id": "5", "result_id": "1"}],
+  "points": "1",
+  "rang": "1"
+};
+var statement2={
+  "statement_id": "2",
+  "statement_text": "Es gibt rechtliche Bedenken hinsichtlich der Auslagerung der Daten.",
+  "options": [{"option_id": "1", "result_id": "1"}, {"option_id": "2", "result_id": "3"},
+    {"option_id": "3", "result_id": "1"},{"option_id": "4", "result_id": "1"},
+    {"option_id": "5", "result_id": "1"}],
+  "points": "2",
+  "rang": "2"
+};
+
+var statement3={
+  "statement_id": "3",
+  "statement_text": "Es besteht Bedarf an Mobilität für die Nutzer der Anwendung.",
+  "options": [{"option_id": "1", "result_id": "1"}, {"option_id": "2", "result_id": "3"},
+    {"option_id": "3", "result_id": "1"},{"option_id": "4", "result_id": "1"},
+    {"option_id": "5", "result_id": "1"}],
+  "points": "2",
+  "rang": "3"
+};
+var option1={
+  "option_id": "1",
+  "option_text": "Trifft zu",
+  "valuation": "100"
+};
+var option2={
+  "option_id": "2",
+  "option_text": "Trifft eher zu",
+  "valuation": "65"
+};
+var option3={
+  "option_id": "3",
+  "option_text": "Trifft eher nicht zu",
+  "valuation": "35"
+};
+var option4={
+  "option_id": "4",
+  "option_text": "Trifft nicht zu",
+  "valuation": "0"
+};
+var option5={
+  "option_id": "5",
+  "option_text": "Keine Angabe"
+};
+
+var result1={
+  "result_id": "1",
+  "result_text": "text"
+};
+var result2={
+  "result_id": "2",
+  "result_text": "text"
+};
+var result3={
+  "result_id": "3",
+  "result_text": "text"
+};
+var statements=[statement1, statement2, statement3];
+var ops=[option1, option2, option3, option4, option5];
+var results=[result1, result2, result3];
+
