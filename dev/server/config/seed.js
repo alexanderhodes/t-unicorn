4bd16315809ae1f1b5e61a31a742f5fcfcecb36d
Vladimir Doroch <s144513@hft-leipzig.de>
2017-05-08 10:01:34 +0200

    Neue Dummy-Daten eingefügt.

--- dev/server/config/seed.js
+++ dev/server/config/seed.js
@@ -24,7 +24,11 @@ Option.find({}).remove()
   },{
   _id: '617364617364616473736462',
   option_text: 'Superkacke.',
-  valuation: 123
+  valuation: 15
+  },{
+    _id: '617364617364616473736469',
+    option_text: 'Superegal.',
+    valuation: 14
   });
 });
 
@@ -37,6 +41,9 @@ Result.find({}).remove()
   },{
     _id: '617364617364616473736465',
     result_text: 'Bad!'
+  },,{
+    _id: '617364617364616473736468',
+    result_text: 'Whatever!'
   });
 });
 
@@ -44,12 +51,26 @@ Statement.find({}).remove()
   .then(() => {
   Statement.create({
     _id: '617364617364616473736463',
-    statement_text: 'asdasdad',
+    statement_text: 'Wer das liest ist doof.',
     statement_rank: 1,
     options: [{option_id: '617364617364616473736461', result_id:'617364617364616473736464'},
               {option_id: '617364617364616473736462', result_id:'617364617364616473736465'}],
-    points: 12123
-  });
+    points: 6
+  },{
+  _id: '617364617364616473736466',
+  statement_text: 'Wer das nicht liest ist doof.',
+  statement_rank: 2,
+  options: [{option_id: '617364617364616473736461', result_id:'617364617364616473736464'},
+    {option_id: '617364617364616473736462', result_id:'617364617364616473736465'}],
+  points: 3
+},{
+  _id: '617364617364616473736467',
+  statement_text: 'Wer sich das vorlesen lässt ist doof.',
+  statement_rank: 3,
+  options: [{option_id: '617364617364616473736461', result_id:'617364617364616473736464'},
+    {option_id: '617364617364616473736462', result_id:'617364617364616473736465'}],
+  points: 66
+});
 });
 
 User.find({}).remove()
