a53b7727d0e45f4799c1286fecda7ee0b0ec3ace
lizakanitz <Kudinova22>
2017-05-15 01:04:03 +0200

    Zum Ergebnis Button eingebaut

--- dev/client/JS/crc.js
+++ dev/client/JS/crc.js
@@ -36,9 +36,6 @@ $(document).ready(function () {
    */
   $("#btn_start_check").on('click touch', function () {
     //setSession
-
-    // console.log("TEST");
-
     sessionStorage.clear();
     window.sessionStorage.setItem('options', '');
     options = "";
@@ -61,6 +58,8 @@ $(document).ready(function () {
     //var progress_circles;
     //progress_circles = setProgressCircles(q.length, currentStatement.rang);
 
+
+
     var card_layout;
     var card_grid_definition = "<div class=\"mdl-grid\">" +
       "<div class=\"mdl-cell--12-col mdl-card mdl-shadow--2dp card_recom\" id=\"card_grid_content\">";
@@ -70,9 +69,6 @@ $(document).ready(function () {
 
 
     var slider_for_options = "<div class=\"slider_area\"><div id=\"slider\"></div><div id=\"list_of_options\">";
-    // for (var i = 0; i < currentStatement.options.length; i++)
-    //  {
-    //  var option_id = i + 1;
     var option_step = 0;
 
     for (var j = 0; j < ops.length; j++) {
@@ -98,6 +94,7 @@ $(document).ready(function () {
     $("#main_content").html(card_layout);
 
 
+
     $(".option_with_step").each(function () {
       var padding_from_left = getActualPaddingFromLeft($(this).attr("option_id"));
       $(this).css("padding-left", padding_from_left)
@@ -111,6 +108,12 @@ $(document).ready(function () {
         "<i class=\"material-icons arrow_back\">arrow_back</i>Zurück " +
         "</button>");
     }
+    if (currentStatement.rang == statements.length) {
+      $(".option_back").html($(".option_back").html() + "<button class=\"mdl-button mdl-js-button mdl-button--raised show_result\" >" +
+        "<i class=\"material-icons arrow_forward\">arrow_forward</i>Zum Ergebnis " +
+        "</button>");
+    }
+
 
     $(".statement_back").on('click touch', function () {
 
@@ -140,61 +143,28 @@ $(document).ready(function () {
 
 
       var next_statement = parseInt($(".mdl-card__title[statement_id]").attr("statement_id"));
-      setTimeout(function () {
-
-        getStatementsLayout(statements[next_statement]);
-      }, 200);
-    });
 
 
-    $(".option_card").one('click touch', function () {
-      //(id_option ins sessionStorage bei der Optionauswahl speichern)
-
-      var next_statement = currentStatement.rang;
-
-      if ($(".show_result").length() == 0) {
+      if ($(".show_result").length == 0) {
         if (sessionStorage.options != "") {
           //trennzeichen zum splitten
           sessionStorage.options += "~";
         }
         sessionStorage.options += $(this).attr("id");
 
-        $(this).css("background", "#CCCCCC");
-        $(this).html($(this).html() + "<i class=\"material-icons done_icon\" style=color:#E20074!important>done</i>");
       }
-      if (currentStatement.rang < q.length) {
+      if (currentStatement.rang < ops.length) {
 
         setTimeout(function () {
-          getStatementsLayout(q[next_statement]);
+          getStatementsLayout(statements[next_statement]);
         }, 300);
       }
 
-      //show "zum Ergebnis" button
-      if (currentStatement.rang == q.length && $(".show_result").length == 0) {
-
-        $(".option_back").html($(".option_back").html() + "<button class=\"mdl-button mdl-js-button mdl-button--raised show_result" + "\">" +
-          "Zum Ergebnis <i class=\"material-icons arrow_back\">arrow_forward</i>" +
-          "</button>");
-        $(".statement_back").on('click touch', function () {
-
-          var previousNumber = $(this).attr("goto");
-          setTimeout(function () {
-            getStatementsLayout(q[previousNumber - 1]);
-          }, 200);
-          var options = sessionStorage.options.split('~');
-          var last_option = options[options.length - 1];
-          var double_last_option = options[options.length - 2];
-          sessionStorage.options = sessionStorage.options.substr(0, sessionStorage.options.length - last_option.length - double_last_option.length - 2);
-
-          setTimeout(function () {
-            $(".option_card[id='" + last_option + "']").addClass("option_clicked");
-            $(".option_card[id='" + last_option + "']").html($(".option_card[id='" + last_option + "']").html() + "<i class=\"material-icons done_icon\">done</i>");
-          }, 200);
-        });
-
-      }
-
     });
+
+
+
+
 
     $("#slider").slider({
       step: 25, change: function (event, ui) {
