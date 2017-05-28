e5428597db477541571abe8da483b65302cd529e
TobiKlee24 <s144223@hft-leipzig.de>
2017-05-28 16:07:47 +0200

    Daumen

--- dev/client/JS/crc.js
+++ dev/client/JS/crc.js
@@ -250,7 +250,7 @@ function show_result(r){
       var Text = r[i].chance_text;
       if (Text != "") {
         lfdNr += 1;
-        box1 += '<div class="data"><div class="bild"><i class="material-icons">pan_tool</i></div><div class="dataheader1">' + lfdNr + '.Vorteil</div><div class="datatext">' + Text +  '</div></div>';
+        box1 += '<div class="data"><div class="thumb_icon"><img class="Thumbs" src="Images/Thumb_Up.png" alt="Daumen hoch"</img></div><div class="dataheader1">' + lfdNr + '.Vorteil</div><div class="datatext">' + Text +  '</div></div>';
       }
     }}
   box1 += '</div>';
@@ -260,8 +260,9 @@ function show_result(r){
     var Text = r[i].risk_text;
     if (Text != "") {
       lfdNr += 1;
-     box2 += '<div class="data"><div class="bild"><img src="Images\Thumb_Down.png" alt="Daumen runter"></div><div class="dataheader2">' + lfdNr + '.Risiko</div><div class="datatext">' + Text + '</div></div>';
-     box2 += '<br><br>';
+     box2 += '<div class="data"><div class="thumb_icon"><img class="Thumbs" src="Images/Thumb_Down.png" alt="Daumen runter"</img></div><div class="dataheader2">' + lfdNr + '.Risiko</div><div class="datatext">' + Text + '</div></div>';
+      box2 += '<br><br>';
+
     }
   }
   result += box1 + box2 + '</div></div>';
