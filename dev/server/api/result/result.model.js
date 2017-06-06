fc7540f20290572ea2c97b70ec670749e9fd8c65
Vladimir Doroch <s144513@hft-leipzig.de>
2017-06-06 09:46:10 +0200

    Bug, dass Kurztexte nicht angezeigt wurden wurde behoben.

--- dev/server/api/result/result.model.js
+++ dev/server/api/result/result.model.js
@@ -4,7 +4,9 @@ import mongoose from 'mongoose';
 
 var ResultSchema = new mongoose.Schema({
   risk_text: [String],
-  chance_text: [String]
+  chance_text: [String],
+  risk_short_text: [String],
+  chance_short_text: [String]
 });
 
 export default mongoose.model('Result', ResultSchema);
